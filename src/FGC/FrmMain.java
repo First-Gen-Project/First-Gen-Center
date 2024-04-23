6479b07fc24e3cceb1cbf4b669f07a894dfdb125
NoahVW <136670476+NoahVW@users.noreply.github.com>
2024-04-23 09:46:20 -0500

    fgc name

--- src/FGC/FrmMain.java
+++ src/FGC/FrmMain.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/GUIForms/JFrame.java to edit this template
  */
-package FGC;
+package fgc;
 
 import bo.Student;
 import dao.StudentHandler;
