bbda93a9a1155e8e0a633d7311d70871ce352dc7
DerekMinchew <minchewderek7@outlook.com>
2024-04-30 20:14:01 -0500

    Corrected GDD, RS, and ERD
    Made ERD, GDD, and RS all have correlating information.

--- src/bo/FirstGenTables.sql
+++ src/bo/FirstGenTables.sql
@@ -42,21 +42,22 @@ CREATE TABLE IF NOT EXISTS notes (
 
 -- Create the Event table
 CREATE TABLE IF NOT EXISTS event (
-    samID INT,
+    eventID INT,
     e_name VARCHAR(255),
+    samID INT,
     date DATE,
-    PRIMARY KEY (samID, e_name),
+    PRIMARY KEY (eventID, e_name),
     FOREIGN KEY (samID) REFERENCES person(samID)
 );
 
 -- Create junction table student_event for event table
 CREATE TABLE IF NOT EXISTS student_event (
-    student_id INT,
-    event_id_samID INT,
-    event_id_e_name VARCHAR(255),
-    PRIMARY KEY (student_id, event_id_samID, event_id_e_name),
-    FOREIGN KEY (student_id) REFERENCES student(samID),
-    FOREIGN KEY (event_id_samID, event_id_e_name) REFERENCES event(samID, e_name)
+    samID INT PRIMARY KEY,
+    student_attend VARCHAR(255),
+    eventID INT,
+    e_name VARCHAR(255),
+    FOREIGN KEY (samID) REFERENCES student(samID),
+    FOREIGN KEY (eventID, e_name) REFERENCES event(eventID, e_name)
 );
 
 -- Create the Application table
@@ -76,13 +77,27 @@ CREATE TABLE IF NOT EXISTS application_info (
 
 -- INSERT INTO student(samID, name, major, minor, gpa, mtrId) VALUES ('33994', 'tom', 'science', 'food', '3.55', '89239'); 
 -- Dummy Data Queries
-
-insert into person values(1, 'Mentor1', 'mentor1@firstgen.com', '2000-01-01');
-insert into mentor values(1, 'mentor1', 'mentor1');
+insert into person values(000376298, 'Dougathy Gilbert', 'dgilbert@shsu.edu', '2004-04-27');
+insert into mentor values(000376298, 'dGilbert', 'PhamLover<3!');
+insert into person values(000289736, 'Gary Dinglesnorpee', 'gqd123@shsu.edu', '2015-06-10');
+insert into student values(000289736, 'Theatre Arts', 'Criminal Justice', '2.01', '000376298');
+insert into person values(000434461, 'Marlin Tower II', 'mxt092@firstgen.com', '2001-12-09');
+insert into mentor values(000434461, 'mTower2', 'Seven7Seven$$');
+insert into person values(000897239, 'Betsy Hepititus', 'student1@firstgen.com', '1969-06-09');
+insert into student values(000897239, 'Criminal Justice', 'N/A', '1.52', '000434461');
+insert into person values(000234768, 'Peter Griffin', 'plg523@shsu.edu', '1966-07-18');
+insert into mentor values(000234768, 'pGriffin', 'ShutUpMeg*1738');
+insert into person values(000987546, 'Conye Northsouth', 'con@firstgen.com', '1984-03-16');
+insert into student values(000987546, 'Political Science', 'Rapping', '3.94', '000234768');
+insert into event values(1, 'Party Rokkers', 000376298, '2005-01-01');
+insert into student_event values(000289736, 'Yes', 1, 'Party Rokkers');
 
 update mentor set password = 'dzZpLdLX5wJ3ZQVlVNgEMIAgohibt2k444Gqt6PuUlQ='
 where samID = 1;
 
+
 select * from mentor;
 select * from student;
 select * from person;
+select * from event;
+select * from student_event;
